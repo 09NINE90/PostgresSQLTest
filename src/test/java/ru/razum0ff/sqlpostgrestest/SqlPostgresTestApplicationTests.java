e68e2f7d3604f37b25378e081a6a5e18541b0270
09NINE90 <razumov.aleksei101@yandex.ru>
2024-09-27 19:13:24 +0500

    init

---
+++ src/test/java/ru/razum0ff/sqlpostgrestest/SqlPostgresTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.razum0ff.sqlpostgrestest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlPostgresTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
